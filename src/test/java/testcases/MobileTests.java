f179a57df42b996fdc431537b32fbae8265e3410
durgasri92 <mdurgasri92@gmail.com>
2023-01-14 18:19:22 +0000

    adding code

---
+++ src/test/java/testcases/MobileTests.java
@@ -0,0 +1,12 @@
+package testcases;
+
+public class MobileTests extends BaseTest {
+
+	
+	protected void setUp() {
+
+		// driver.setDimension(400*600);
+	}
+}
+
+
---
+++ src/test/java/testcases/SearchTests.java
@@ -0,0 +1,20 @@
+package testcases;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import utils.DataUtils;
+
+public class SearchTests extends BaseTest {
+
+	@Test
+	public void testSearchCourses() {
+
+		homepage.clickElearningMenuItem();
+
+		AssertJUnit.assertEquals(searchPage.searchAndExtractResults(DataUtils.getTestData("Config","SearchCourseKeyword")).stream()
+				.allMatch(text -> text.toLowerCase().contains(DataUtils.getTestData("Config","SearchCourseKeyword"))), true);
+
+	}
+
+}
